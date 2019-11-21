0166d2fb5c95cc048aeeb21dea2a48394bd08e4a
Victor moreau <victor.moreau@sogeti.com>
2019-11-21 16:36:31 +0100

    SETUP Env

---
+++ src/main/java/Services.java
@@ -0,0 +1,27 @@
+import com.sun.tools.javac.util.Log;
+import java.util.function.Consumer;
+
+import static spark.Spark.*;
+
+public class Services {
+
+    public static void main(String[] args) {
+
+        get("/hello", (req, res) -> "Hello, world");
+
+        get("/hello/:name", (req,res) -> {
+            return "Hello, "+ req.params(":name");
+        });
+
+        get("/hello/:name", (request, response) -> {
+            return "Hello: " + request.params(":name");
+        });
+
+    }
+
+    private Consumer<Exception> initExceptionHandler = (e) -> {
+        System.out.println("An erreur occured");
+        System.exit(100);
+    };
+
+}
