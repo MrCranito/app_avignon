bd27b192cf4ec2d4b605c23c5fccb904be0125df
Victor moreau <victor.moreau@sogeti.com>
2019-11-21 16:26:51 +0100

    Upate folderes moddels

--- src/main/java/ItinaryService.java
+++ src/main/java/ItinaryService.java
@@ -1,6 +1,11 @@
+import com.sun.tools.javac.util.Log;
+
+import java.util.function.Consumer;
+
 import static spark.Spark.*;
 
 public class ItinaryService {
+
     public static void main(String[] args) {
 
         get("/hello", (req, res) -> "Hello, world");
@@ -8,5 +13,16 @@ public class ItinaryService {
         get("/hello/:name", (req,res) -> {
             return "Hello, "+ req.params(":name");
         });
+
+        get("/hello/:name", (request, response) -> {
+            return "Hello: " + request.params(":name");
+        });
+
     }
+
+    private Consumer<Exception> initExceptionHandler = (e) -> {
+        System.out.println("An erreur occured");
+        System.exit(100);
+    };
+
 }
